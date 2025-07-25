3b548bfd40166fdad3a2b6f56b233204a6e26ea3
miqayelyan778 <minasmiqayelyan001@gmail.com>
2025-07-24 23:35:29 -0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Dash Notifier Bot is running!"
 
 def run():
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
